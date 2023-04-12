69afc4efb03c912d9e83470c9ca4a5ac52a1a35f
rprad <rprad@DESKTOP-AMGHNDL>
2023-04-12 11:46:17 +0530

    commit n push

--- src/test/java/sample/demo.java
+++ src/test/java/sample/demo.java
@@ -1,5 +1,20 @@
 package sample;
 
+import org.testng.annotations.Test;
+
 public class demo {
+	@Test
+	public void demoo()
+	
+	{
+		System.out.println("this is a demo");
+		System.out.println("hiiii");
+		
+	}
+	@Test
+	public void demo1() {
+		System.out.println("this is demo");
+	}
+	
 
 }
